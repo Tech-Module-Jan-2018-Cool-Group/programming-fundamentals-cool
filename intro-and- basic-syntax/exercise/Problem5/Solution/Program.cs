193bcb9c30412d390a3c73733d27b58e583a5ac6
Karinacho <kreativen_dom@abv.bg>
2018-03-16 20:43:30 +0200

    Added Problem5 solution!

---
+++ intro-and- basic-syntax/exercise/Problem5/Solution/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currh = int.Parse(Console.ReadLine());
+            int maxh = int.Parse(Console.ReadLine());
+            int curre = int.Parse(Console.ReadLine());
+            int maxe = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Health: " + "|" + new string('|', currh) + new string('.', maxh - currh) + "|");
+            Console.WriteLine("Energy: " + "|" + new string('|', curre) + new string('.', maxe - curre) + "|");
+
+        }
+    }
+}
