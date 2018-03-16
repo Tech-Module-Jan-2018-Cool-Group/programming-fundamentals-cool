8b38f5ab24926c96ab7f1f3013ebb9314db7543e
i_villani <navisdays@gmail.com>
2018-03-16 18:24:25 +0200

    Added Problem1 solution!

---
+++ intro-and- basic-syntax/exercise/Problem1/Solution/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebitCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gogo = new List<string>();
+            for (int i = 1; i <= 4; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                gogo.Add(a.ToString());
+            }
+            for (int i = 0; i < 4; i++)
+            {
+
+                Console.Write(new string('0', 4 - gogo[i].Length) + gogo[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
