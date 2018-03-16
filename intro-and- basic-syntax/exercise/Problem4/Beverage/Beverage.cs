a7cdf9dfca820e8409ded53a2b313bbd78d39f7e
Bobi <borislavjs@gmail.com>
2018-03-16 20:43:09 +0200

    Added Problem4 solution!

---
+++ intro-and- basic-syntax/exercise/Problem4/Beverage/Beverage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Beverage
+{
+class Beverage
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        double energy = a * (b * 0.01);
+        double sugar = a * (c * 0.01);
+
+        Console.WriteLine(a + "ml" + " " + name + ":");
+        Console.WriteLine(energy + "kcal, " + sugar + "g sugars");
+    }
+}
+}
